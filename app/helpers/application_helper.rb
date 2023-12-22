935c0fc3a5744415bd06a57d172a289e662da6ca
espoir <espoir1018@outlook.com>
2023-12-22 08:53:01 +0800

    bootstrap-icon 引入 SVG 的方式

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def bs_icon_tag(icon, **options)
+    content_tag :svg, options do
+      content_tag :use, nil, 'xlink:href': "bootstrap-icons.svg##{icon}"
+    end
+  end
+
 end
