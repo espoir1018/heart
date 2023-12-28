b5b97c79874115282ce6df57b54d9e3197b3e4f2
espoir <espoir1018@outlook.com>
2023-12-28 09:53:31 +0800

    引入 bootstrap 插件

--- app/javascript/bootstrap.main.js
+++ app/javascript/bootstrap.main.js
@@ -1,5 +1,7 @@
 import * as bootstrap from "bootstrap"
 
+window.bootstrap = bootstrap
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
